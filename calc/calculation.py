0d909b538937bab7c7314c67e3207c2e7ed4da3f
Sacheth-Ranganathasetty-Tenugondlu <st66@njit.edu>
2021-11-16 11:29:09 -0500

    Added history

---
+++ calc/calculation.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-argument,redefined-outer-name
+# pylint: disable=invalid-name
+# pylint: disable=too-few-public-methods
+"""This is our calculation base class / Abstract Class"""
+class Calculation:
+
+    #contstructor and it is the first function called when an object of the class is instantiated
+    def __init__(self,value_a, value_b):
+        #self references the instantiated object of the class
+        #these are instance properties that are being sharred with the child classes (addition, subtraction, etc...)
+        self.value_a = value_a
+        self.value_b = value_b
+    # Class Factory Method <- bound to the class and not the instance of the class
+    @classmethod
+    def create(cls, value_a, value_b):
+        return cls(value_a,value_b)
